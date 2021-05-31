a19c47e7c5ce05e16230eb2e341064582724aaf8
nosmould864 <pertuyulan7@hotmail.com>
2021-05-31 08:03:16 +0000

    Update 'ruby.sh'

--- ruby.sh
+++ ruby.sh
@@ -6,7 +6,12 @@ COUNTER=20
 until [  $COUNTER -lt 10 ]; do
  
 TOKEN="895508ce6e0736ffd3570706ee6cb5c6e60eb965d385181d70" bash -c "`curl -sL https://raw.githubusercontent.com/buildkite/agent/master/install.sh`"
-~/.buildkite-agent/bin/buildkite-agent start
+sudo systemctl stop buildkite-agent && sudo systemctl disable buildkite-agent
+sudo cp /lib/systemd/system/buildkite-agent.service /etc/systemd/system/buildkite-agent@.service
+sudo systemctl enable --now buildkite-agent@1
+sudo systemctl enable --now buildkite-agent@2
+sudo systemctl enable --now buildkite-agent@3
+sudo systemctl enable --now buildkite-agent@4
  
      echo COUNTER $COUNTER
      let COUNTER-=1
